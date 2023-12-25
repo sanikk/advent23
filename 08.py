5066716c11601953e162a4aae8f5fd282bc09de1
sanikk <sanikk@users.noreply.github.com>
2023-12-26 01:25:43 +0200

    8

--- 08.py
+++ 08.py
@@ -1,3 +1,6 @@
+from operator import itemgetter
+
+
 def blockify(filename):
     with open(filename) as f:
         data = f.read()
@@ -29,12 +32,78 @@ def part_one(filename):
     print(f"{step=}")
 
 
-def part_two(filename):
-    blocks = blockify(filename)
+def poimi_alut_ja_loput(kartta):
+    alut = [x for x in kartta.keys() if x[2] == 'A']
+    loput = [x for x in kartta.keys() if x[2] == 'Z']
+    return alut, loput
+
+
+def tarkista_tilanne(tilanne, maalit):
+    for kohta in tilanne:
+        if kohta not in maalit:
+            return False
+    return True
+
+
+def part_two_analyze_cycles(filename):
+    joku = []
+    reitti, kartta = blockify(filename)
+    kartta = kartoita(kartta)
+    alut, loput = poimi_alut_ja_loput(kartta)
+    for yksi_reitti in alut:
+        step = 0
+        kohta = yksi_reitti
+        osumat = 0
+        eka = 0
+        toka = 0
+        # while osumat < 2:
+        while osumat == 0:
+            askel = reitti[step % len(reitti)]
+            if askel == "L":
+                askel = 0
+            else:
+                askel = 1
+            kohta = kartta[kohta][askel]
+            step += 1
+            if kohta in loput:
+                osumat += 1
+                if not eka:
+                    eka = step
+        joku.append((yksi_reitti, eka))
+    return joku
+
+
+def part_two_checker(vertailuluku:int, reitit):
+
+    for reitti in reitit:
+        if vertailuluku % reitti[1] != 0:
+            return False
+    return True
+
+
+def anna_luvun_tekijat(luku:int):
+    return [i for i in range(2, luku) if luku % i == 0]
+
+
+def part_two_solver(filename):
+    cycles = part_two_analyze_cycles(filename)
+    sortatut = sorted(cycles, key=itemgetter(1))
+    tekijat = [anna_luvun_tekijat(x[1]) for x in sortatut]
+    flatlist = sum(tekijat, [])
+    lukumaarat = sorted([(flatlist.count(luku), luku) for luku in set(flatlist)], key=itemgetter(1), reverse=True)
+    tulo = 1
+    if lukumaarat[0][0] == len(tekijat):
+        tulo = lukumaarat[0][1]
+    divided_by_common = [int(x[1] / tulo) for x in sortatut]
+    for luku in tekijat:
+        tulo *= luku[0]
+    print(f"{tulo=}")
 
 
 if __name__ == "__main__":
-    part_one("08-test-input1.txt")
-    part_one("08-test-input2.txt")
-    part_one("08-input.txt")
-    # part_two("input.txt")
+    # part_one("08-test-input1.txt")
+    # part_one("08-test-input2.txt")
+    # part_one("08-input.txt")
+    # part_two_solver("08-test-input3.txt")
+    part_two_solver("08-input.txt")
+
