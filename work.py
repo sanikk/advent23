55e47607ea5e9519d22de8035fc5f2d49bbca50d
sanikk <sanikk@users.noreply.github.com>
2023-12-26 09:18:26 +0200

    kartta valmis, jatkuu parin tunnin päästä

--- work.py
+++ work.py
@@ -70,18 +70,52 @@ def part_one(filename):
     tulos = int(len(reitti) / 2)
     print(f"{tulos=}")
 
+def etsi_torit(reitti, blocks):
+    map_symbols = {"|": "NS", "-": "EW", "L": "NE", "J": "NW", "7": "SW", "F": "SE"}
+    sijainti = reitti[0]
+    while sijainti != reitti[-1]:
+        rivi, sarake = sijainti
+        nykyinen = blocks[sijainti[0]][sijainti[1]]
+        suunnat = map_symbols[nykyinen]
+        if "N" not in suunnat and blocks[sijainti[0] - 1][sijainti[1]] == ".":
+            pass
+        tutkailtava = (rivi - 1, sarake)
+
+        tutkailtava = (rivi + 1, sarake)
+
+
+        tutkailtava = (rivi, sarake - 1)
+
+
+        tutkailtava = (rivi, sarake + 1)
+
+def piirra_kartta(reitti, blocks):
+    pienin_x = min([x[0][1] for x in reitti])
+
+    isoin_x = max([x[0][1] for x in reitti])
+    pienin_y = min([x[0][0] for x in reitti])
+    isoin_y = max([x[0][0] for x in reitti])
+    print(f"{pienin_y=}, {isoin_y=}")
+    kartta = blocks[pienin_y:isoin_y + 1]
+    kartta = [rivi[pienin_x:isoin_x+1] for rivi in kartta]
+    with open("output.txt", 'w') as f:
+        for rivi in kartta:
+            print(rivi)
+            f.write(rivi+"\n")
 
 def part_two(filename):
     blocks = blockify(filename)
-
+    animal = find_animal(blocks)
+    reitti = actually_follow(animal, blocks)
+    piirra_kartta(reitti, blocks)
 
 if __name__ == "__main__":
-    part_one("10-test-input1.txt")
+    # part_one("10-test-input1.txt")
     # part_one("10-test-input2.txt")
     # part_one("10-test-input3.txt")
     # part_one("10-test-input4.txt")
-    part_one("10-input.txt")
-
+    # part_one("10-input.txt")
+    part_two("10-test-input-5.txt")
 # map_symbols = {"|": "N-S", "-": "E-W", "L": "N-E", "J": "N-W", "7": "S-W", "F": "S-E"}
 # .     No pipe
 # S     Animal starts here
