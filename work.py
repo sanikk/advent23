52a2ee3ac536dbd85b8aa6f55754b5f84b6f38c3
sanikk <sanikk@users.noreply.github.com>
2023-12-27 01:20:40 +0200

    kymppi kesken, siellä on lisää sääntöjä. oispa pidempiä tehtävänantoja..

--- work.py
+++ work.py
@@ -55,9 +55,8 @@ def actually_follow(start, blocks):
     reitti = [start]
     while sijainti != start:
         reitti.append(sijainti)
-        # eka mukaan vaan jos reittiä on tarpeeks
         if len(reitti) > 2:
-            sijainti = seuraa_putkea(sijainti, blocks,reitti[0], reitti[-2])
+            sijainti = seuraa_putkea(sijainti, blocks, reitti[0], reitti[-2])
         else:
             sijainti = seuraa_putkea(sijainti, blocks, None, reitti[-2])
     return [(sijainti, blocks[sijainti[0]][sijainti[1]]) for sijainti in reitti]
@@ -70,44 +69,62 @@ def part_one(filename):
     tulos = int(len(reitti) / 2)
     print(f"{tulos=}")
 
-def etsi_torit(reitti, blocks):
-    map_symbols = {"|": "NS", "-": "EW", "L": "NE", "J": "NW", "7": "SW", "F": "SE"}
-    sijainti = reitti[0]
-    while sijainti != reitti[-1]:
-        rivi, sarake = sijainti
-        nykyinen = blocks[sijainti[0]][sijainti[1]]
-        suunnat = map_symbols[nykyinen]
-        if "N" not in suunnat and blocks[sijainti[0] - 1][sijainti[1]] == ".":
-            pass
-        tutkailtava = (rivi - 1, sarake)
-
-        tutkailtava = (rivi + 1, sarake)
+#    map_symbols = {"|": "NS", "-": "EW", "L": "NE", "J": "NW", "7": "SW", "F": "SE"}
+#        tutkailtava = (rivi - 1, sarake)
 
 
-        tutkailtava = (rivi, sarake - 1)
+def poista_ulkopuoli(kartta, reitin_pisteet):
+    # Oletetaan että ulkopuoli on yhtänäinen, kuten tehtävissä, jokaisesta pisteestä on pääsy jokaiseen pisteeseen.
+    # Ison kartan alue rajoittuu oikeasta reunasta seinään, mutta jokaiseen ulkopuolen pisteeseen pääsee edelleen
+    # kiertämällä.
+
+    # tjaahas tuo squeeze unohtui, eli pitää vähän lisätä sinne sääntöä. tarkistetaan onko reitin putkea vai ei,
+    # piirtämisen varalta, mutta tungetaan kuitenkin pinoon kun se tunkee sieltä välistä
+    # pistelasku myös väärin, ilmeisesti lasketaan myös ruudut joissa on luuppiin liittämättömiä putkia.
+
+    pino = [(0, 0)]
+    maski = [[0 for merkki in kartta[0]] for rivi in kartta]
+    while pino:
+        rivi, sarake = pino.pop()
+        maski[rivi][sarake] = 1
+        ylhaalla = (rivi - 1, sarake)
+        vasemmalla = (rivi, sarake - 1)
+        oikealla = (rivi, sarake + 1)
+        alhaalla = (rivi + 1, sarake)
+        if rivi > 0 and ylhaalla not in reitin_pisteet and maski[rivi - 1][sarake] != 1:
+            pino.append(ylhaalla)
+        if sarake > 0 and vasemmalla not in reitin_pisteet and maski[rivi][sarake - 1] != 1:
+            pino.append(vasemmalla)
+        if rivi < len(kartta) - 1 and alhaalla not in reitin_pisteet and maski[rivi + 1][sarake] != 1:
+            pino.append(alhaalla)
+        if sarake < len(kartta[0]) - 1 and oikealla not in reitin_pisteet and maski[rivi][sarake + 1] != 1:
+            pino.append(oikealla)
+    return maski
 
 
-        tutkailtava = (rivi, sarake + 1)
-
-def piirra_kartta(reitti, blocks):
-    pienin_x = min([x[0][1] for x in reitti])
-
-    isoin_x = max([x[0][1] for x in reitti])
-    pienin_y = min([x[0][0] for x in reitti])
-    isoin_y = max([x[0][0] for x in reitti])
-    print(f"{pienin_y=}, {isoin_y=}")
-    kartta = blocks[pienin_y:isoin_y + 1]
-    kartta = [rivi[pienin_x:isoin_x+1] for rivi in kartta]
-    with open("output.txt", 'w') as f:
-        for rivi in kartta:
-            print(rivi)
-            f.write(rivi+"\n")
+def siisti_kartta(reitin_pisteet, kartta):
+    kartta = [[merkki for merkki in rivi] for rivi in kartta]
+    siistimpi_kartta = poista_ulkopuoli(kartta, reitin_pisteet)
+    loppukartta = [['.' for merkki in kartta[0]] for rivi in kartta]
+    for rivi in range(len(kartta)):
+        for sarake in range(len(kartta[0])):
+            if siistimpi_kartta[rivi][sarake] == 1:
+                loppukartta[rivi][sarake] = '0'
+            else:
+                loppukartta[rivi][sarake] = kartta[rivi][sarake]
+    return loppukartta
 
 def part_two(filename):
     blocks = blockify(filename)
+    blocks = [rivi for rivi in blocks if rivi]
     animal = find_animal(blocks)
     reitti = actually_follow(animal, blocks)
-    piirra_kartta(reitti, blocks)
+    loppukartta = siisti_kartta([x[0] for x in reitti], blocks)
+    tulos = 0
+    for rivi in loppukartta:
+        tulos += rivi.count(".")
+    print(f"{tulos=}")
+
 
 if __name__ == "__main__":
     # part_one("10-test-input1.txt")
@@ -116,6 +133,7 @@ if __name__ == "__main__":
     # part_one("10-test-input4.txt")
     # part_one("10-input.txt")
     part_two("10-test-input-5.txt")
+    # part_two("10-input.txt")
 # map_symbols = {"|": "N-S", "-": "E-W", "L": "N-E", "J": "N-W", "7": "S-W", "F": "S-E"}
 # .     No pipe
 # S     Animal starts here
