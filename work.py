e559e1e37f447d9e30e4ab95a5dc641245a79231
sanikk <sanikk@users.noreply.github.com>
2024-01-01 03:12:31 +0200

    day 15 fail 1

--- work.py
+++ work.py
@@ -1,10 +1,39 @@
 def blockify(filename):
     with open(filename) as f:
-        return f.read().split("\n")
+        return f.read()
+
+
+def HASHfunction_ii(feed: str):
+    current = 0
+    for merkki in feed:
+        current += ord(merkki)
+        current *= 17
+        current = current % 256
+        yield current
+
+def HASHfunction(feed: str):
+    current = 0
+    for merkki in feed:
+        if merkki == ",":
+            continue
+        current += ord(merkki)
+        current *= 17
+        current = current % 256
+        yield current
+
+
+def kasittele_patka(patka: str):
+    current = 0
+    for merkki in patka:
+        current += ord(merkki)
+        current *= 17
+        current = current % 256
+    return current
 
 
 def part_one(filename):
     blocks = blockify(filename)
+    print(sum(kasittele_patka(patka) for patka in blocks.split(',')))
 
 
 def part_two(filename):
@@ -12,5 +41,7 @@ def part_two(filename):
 
 
 if __name__ == "__main__":
-    part_one("input.txt")
-    part_two("input.txt")
+    part_one("15-test-input-1.txt")
+    part_one("15-input.txt")
+
+
